91a7fc7dc9273afb8d1aa18544e8f11f3eb6671e
Nass <nasali@anypresence.com>
2015-07-09 09:32:44 -0600

    Tweak test.

--- test/test_deis_client.rb
+++ test/test_deis_client.rb
@@ -2,14 +2,23 @@ require 'minitest/autorun'
 require 'deis_client'
 
 class DeisClientTest < Minitest::Test
+
   def test_login
     client = DeisClient.new(ENV['DEIS_CONTROLLER'] || "http://controller.example.com",
                             ENV['DEIS_USER'] || 'humpty',
                             ENV['DEIS_PASSWORD'] || 'dumpty',
-                            true
+                            ENV['MOCK_CALLS_TO_DEIS']
                            )
-    assert client.user_token == "ABC123"
-    assert client.app_create("fluffy") == {}
+
+    if ENV['MOCK_CALLS_TO_DEIS']
+      assert client.user_token == "ABC123"
+      assert client.app_create("cloudy") == {}
+    else
+      response = client.app_create("fluffy")
+      assert response["id"] == "fluffy"
+      assert response["owner"] == ENV['DEIS_USER'] || 'humpty'
+      assert response["uuid"]
+    end
   end
 
 end
